96d18773639702416847e5eb7a21edbd4aa3c6c2
RightHennessy <myheaven_b@naver.com>
2022-01-20 21:48:18 +0900

    chore : correct binding

--- SE_AL/app/src/main/java/com/example/se_al/ui/AlarmBaseListFragment.kt
+++ SE_AL/app/src/main/java/com/example/se_al/ui/AlarmBaseListFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.se_al.R
+import com.example.se_al.databinding.ActivityMainBinding
 import com.example.se_al.databinding.FragmentAlarmBaseListBinding
 
 class AlarmBaseListFragment  : Fragment() {
@@ -15,6 +20,10 @@ class AlarmBaseListFragment  : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var viewModel : AlarmBaseListViewModel
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +36,10 @@ class AlarmBaseListFragment  : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentAlarmBaseListBinding.inflate(inflater, container, false)
 
+
+        //_binding = FragmentAlarmBaseListBinding.inflate(inflater, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_alarm_base_item, container, false)
         activity?.let {
             viewModel = ViewModelProvider(it).get(AlarmBaseListViewModel::class.java)
             binding.viewModel = viewModel
